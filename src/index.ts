31e5731311a6edd9b969a8820b457cda2b14a60f
zerodice0 <zerodice0@gmail.com>
2022-01-05 01:31:56 +0900

    - apply cross-env.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,18 @@ import Koa from 'koa';
 import Serve from 'koa-static';
 import Router from 'koa-router';
 import BodyParser from 'koa-bodyparser';
+import DotEnv from 'dotenv';
+import path from 'path';
+
+if (process.env.NODE_ENV === 'production') {
+  DotEnv.config({
+    path: path.join(__dirname, '.env.production')
+  });
+} else {
+  DotEnv.config({
+    path: path.join(__dirname, '.env.development')
+  });
+}
 
 const app = new Koa();
 const router = new Router();
@@ -13,5 +25,7 @@ app.use(router.routes);
 app.use(bodyParser);
 
 app.listen(4000, () => {
+  console.log(process.env.NODE_ENV);
+
   console.log('Server listening on port 4000');
 });
