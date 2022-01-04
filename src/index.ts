f801fe0513cd4752feab9837d1535bdb7b5112ac
zerodice0 <zerodice0@gmail.com>
2022-01-04 21:58:29 +0900

    - add koa-static serving to src directory.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import Koa from 'koa';
+import Serve from 'koa-static';
+import Router from 'koa-router';
+import BodyParser from 'koa-bodyparser';
 
 const app = new Koa();
+const router = new Router();
+const serve = Serve(__dirname);
+const bodyParser = BodyParser();
 
-app.use((context: Koa.Context) => {
-  context.body = 'Hello!';
-});
+app.use(serve);
+app.use(router.routes);
+app.use(bodyParser);
 
 app.listen(4000, () => {
   console.log('Server listening on port 4000');
